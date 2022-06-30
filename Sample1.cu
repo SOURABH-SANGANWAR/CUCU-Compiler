3b5df7ac3a8e58205308fd81b2cce3bb257d18cf
SANGANWAR  SOURABH <79750636+SOURABH-SANGANWAR@users.noreply.github.com>
2022-06-30 15:57:21 +0530

    Update example codes
    Added examples for language.
    First one is syntatically correct
    Second one is syntatically incorrect

---
+++ Sample1.cu
@@ -0,0 +1,30 @@
+int x;
+char * y;
+int mau(int j, char *h);
+int a;
+int u;
+int main( )
+{
+    if( a==1==2 || a>2){
+        mau(u,"hello");
+    }
+    if( 3!=2)
+    {
+
+    }
+    else
+    {
+        x = 10;
+        y = "bye";
+        int g=02* mau(u,"hello");
+
+    }
+    while(a+2>0)
+    {
+        if( a==1){
+        main(2);
+    }
+    }
+
+
+}
---
+++ Sample2.cu
@@ -0,0 +1,10 @@
+int x;
+int main(){
+    hell= " bye";
+    int x=20;  
+    if(x>0){
+        x = "BYE";
+    }
+    ....
+}
+}
